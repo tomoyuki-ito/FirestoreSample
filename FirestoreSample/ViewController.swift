557313c39e1976e1d61b43b0b48538b8bab132dc
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-09-05 13:45:41 +0900

    feat: read write test

--- FirestoreSample/ViewController.swift
+++ FirestoreSample/ViewController.swift
@@ -11,20 +11,27 @@ import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    lazy var db: Firestore = {
+        return Firestore.firestore()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let db = Firestore.firestore()
+        addSnapshotListener()
+        setData(name: "ichiro")
+    }
+    
+}
+
+extension ViewController {
+    
+    private func getDocuments() {
         db.collection("users").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
@@ -36,5 +43,50 @@ class ViewController: UIViewController {
         }
     }
     
+    private func whereFieldGetDocuments() {
+        db.collection("users").whereField("isValid", isEqualTo: true).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+    }
+    
+    private func addSnapshotListener() {
+        db.collection("users").addSnapshotListener { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+    }
+    
+    private func whereFieldAddSnapshotListener() {
+        db.collection("users").whereField("isValid", isEqualTo: true).addSnapshotListener() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+    }
+    
+    private func setData(name: String) {
+        db.collection("users").document().setData(["name": name]) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+            }
+        }
+    }
+    
 }
-
